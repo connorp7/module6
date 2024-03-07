2deca58a017d5b1967474954295c8f4063514aff
Connor <connorp7@vt.edu>
2024-03-07 16:23:29 -0500

    last commit of 3/7

--- PixyProxy/data/image_repository.py
+++ PixyProxy/data/image_repository.py
@@ -49,18 +49,18 @@ class MySQLImageRepository(ImageRepositoryInterface):
             raise
         return {"guid": guid, "filename": filename, "prompt": image.prompt}
 
-    def get_image_details_by_guid(self, guid) -> ImageDetail:
+    def get_image_details_by_guid(self, guid: str) -> ImageDetail:
         db_context = get_current_db_context()
         if not db_context:
             raise DBConnectionError("No database connection found")
         try:
-            db_context.cursor.execute("SELECT guid, filename, prompt FROM images WHERE guid = %s", (guid,))
+            db_context.cursor.execute("SELECT guid, filename, prompt FROM images WHERE guid = %s", guid)
             row = db_context.cursor.fetchone()  # fetch the result
         except Exception as e:
             db_context.rollback_transaction()
             raise  # re-raise the exception
         if row:
-            return ImageDetail(guid=row[1], filename=row[2], prompt=row[3])
+            return ImageDetail(guid=row[0], filename=row[1], prompt=row[2])
 
         
     def get_all_images(self) -> list[ImageDetail]:
@@ -74,14 +74,14 @@ class MySQLImageRepository(ImageRepositoryInterface):
             db_context.rollback_transaction()
             raise  # re-raise the exception
 
-        return [ImageDetail(guid=row[1], filename=row[2], prompt=row[3]) for row in rows]
+        return [ImageDetail(guid=row[0], filename=row[1], prompt=row[2]) for row in rows]
 
     def get_image_file(self, guid):
         db_context = get_current_db_context()
         if not db_context:
             raise DBConnectionError()
         try:
-            db_context.cursor.execute("SELECT filename FROM images WHERE guid = %s", (guid,))
+            db_context.cursor.execute("SELECT filename FROM images WHERE guid = %s", guid)
             result = db_context.cursor.fetchone()  # fetch the result
         except Exception as e:
             db_context.rollback_transaction()
